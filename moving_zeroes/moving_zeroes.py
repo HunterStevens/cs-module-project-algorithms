fb2ca8fa2abce7933773a10766ab989e6d0fd45d
Hunter Stevens <hunterstevens1996@gmail.com>
2020-06-17 14:16:05 -0600

    finished single_number and making_zeroes

--- moving_zeroes/moving_zeroes.py
+++ moving_zeroes/moving_zeroes.py
@@ -4,9 +4,14 @@ Returns: a List of integers
 '''
 def moving_zeroes(arr):
     # Your code here
+    count = arr.count(0)
+    print(count)
+    while count > 0:
+        arr.remove(0)
+        arr.append(0)
+        count -=1
 
-    pass
-
+    return arr
 
 if __name__ == '__main__':
     # Use the main function here to test out your implementation
